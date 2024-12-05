4228324a7f80599e05a408176f4bd37134139ed4
Ankita <bobdeankita24@gmail.com>
2024-12-04 23:36:21 -0330

    demo program

---
+++ String/Bubblesort.cpp
@@ -0,0 +1,20 @@
+//demo code in bubble sort
+#include<iostream>
+using namespace std;
+int sum(int arr[],int t)
+{
+    int pluse=0;
+    for(int i=0;i<t;i++)
+    {
+        pluse += arr[i];
+    }
+    return pluse;
+
+}
+int main ()
+{
+    int arr[]={5, 2, 4, 6, 1, 3};
+    int s=sizeof(arr)/sizeof(arr[0]);
+    cout<<sum(arr,s)
+}
+
